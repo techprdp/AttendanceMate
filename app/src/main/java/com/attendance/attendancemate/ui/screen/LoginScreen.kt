b91524545c1911c68045ce4763a027c38035be7c
Techprdp <pradeep.developer1204@gmail.com>
2024-09-29 00:00:16 +0530

    Add ads

--- app/src/main/java/com/attendance/attendancemate/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/attendance/attendancemate/ui/screen/LoginScreen.kt
@@ -35,7 +35,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
